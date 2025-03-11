415ff6d4186e1ed4f824a6f5c05f8613e4b3b5be
Bruno Sales <raccoon6965@gmail.com>
2025-03-11 09:30:04 -0300

    feat: add Photo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 import Counter from './components/Counter';
+import Photo from './components/Photo';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchPhotos = async () => {
+    try {
+      const url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+      const response = await fetch(url); // Por padrão executa uma request do tipo GET
+      console.log(response);
+      if (response.status === 200) {
+        const data = await response.json();
+        // console.log(data);
+        // correção da imagem thumb
+        const updatedPhotos = data.map( (photo) => ({
+          ...photo,
+          thumbnailUrl: `https://picsum.photos/150?random=${photo.id}`
+        }));
+        // ...photo {id: 1, title: "rótulo", thumbnailUrl: "http:///",...}
+        // { photo: {id: 1, title: "rótulo",... }}
+        setPhotos(updatedPhotos);
+      }
+
+    } catch (error) {
+      console.error('Erro ao buscar fotos', error);
+    }
+  }
+  console.log(photos);
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [])
 
   // function updateCount() {
   //   setCount(count+1);
   // }
 
   // arraw function 
+  // () => () return implícito
+  // () => {} // é necessário colocar o return (caso precise)
 
   const updateCount = () => {
     // outros comandos
@@ -39,6 +71,16 @@ function App() {
     <>
       <Counter title="Contando..." />
       <Counter initial="100" />
+      <article>
+        <h1>Álbum da API</h1>
+        {photos.map ( (photo) => (
+          // <article key={photo.id}>
+          //   <h2>ID #{photo.id} {photo.title}</h2>
+          //   <img src={photo.thumnailUrl} alt={photo.title} />
+          // </article>
+          <Photo photo={photo} />
+        ))}
+      </article>
     </>
   )
 }
