f7ee351baafaccd3a5b383d9798bb5bb23835f8d
Bruno Sales <raccoon6965@gmail.com>
2025-08-12 10:10:41 -0300

    navegação básica entre telas

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { ThemeProvider } from './src/contexts/ThemeContext'; // Nova
-
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
+import RootNavigator from './src/navigation/RootNavigation';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <HomeScreen />
+      <RootNavigator />
     </ThemeProvider>
     
   );
