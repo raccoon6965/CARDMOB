e63c5af3dbc7f9db0f403f79ab67e9e0c60f4339
Bruno Sales <raccoon6965@gmail.com>
2025-08-19 08:41:01 -0300

    Inclui novas telas: registro e login

---
+++ Etapa3/loja-react-app/src/navigation/RootNavigator.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RootStackParamList, TabParamList } from './types';
+
+// Telas do App.
+import HomeScreen from "../screens/HomeScreen";
+// importar depois que implementar: DetailsScreen, SettingsScreen
+import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/LoginScreen";
+
+const AppStack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator<TabParamList>();
+
+function TabNavigator() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Settings" component={HomeScreen}/>
+            <Tab.Screen name="Register" component={RegisterScreen}/>
+        </Tab.Navigator>
+    );
+}
+
+function AppNavigator() {
+    return (
+        <AppStack.Navigator>
+            <AppStack.Screen name="Tabs" component={TabNavigator} options={{ headerShown: false }}/>
+            <AppStack.Screen name="Details" component={HomeScreen} options={{ title: 'Details' }}/>
+            <AppStack.Screen name="Login" component={LoginScreen} options={{ title: "Acessar" }} />
+        </AppStack.Navigator>
+    );
+}
+
+export default function RootNavigator() {
+    return (
+        <NavigationContainer>
+            <AppNavigator />
+        </NavigationContainer>
+    );
+};
--- Etapa3/loja-react-app/src/screens/HomeScreen.tsx
+++ Etapa3/loja-react-app/src/screens/HomeScreen.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 import { useTheme } from '../contexts/ThemeContext'; // Nova
 
-function HomeScreen() {
+function HomeScreen({ navigation }: any) {
     const { theme, toggleTheme } = useTheme();
     return (
         <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
             <Text style={{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>Home Screen</Text>
             <Button title="Alternar Tema" color={theme.colors.primary} onPress={toggleTheme}/>
+            <Button title="Ir para Detalhes" onPress={ () => navigation.navigate('Details')} />
+            <Button title="Login" onPress={ () => navigation.navigate('Login')} />    
         </View>
     );
 }
