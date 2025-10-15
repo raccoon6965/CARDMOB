06df5db68d26d97112f842fab497f5a2785738dc
Bruno Sales <raccoon6965@gmail.com>
2025-10-15 10:23:18 -0300

    adição de AuthService

--- Etapa3/loja-react-app/src/services/authService.ts
+++ Etapa3/loja-react-app/src/services/authService.ts
@@ -1,7 +1,7 @@
 import Constants from 'expo-constants'; // novo
 
 // const API_URL = Config.API_URL;
-// const API_URL = 'http://10.81.205.50:5000';
+// const API_URL = 'http://10.81.205.25:5000';
 const { apiUrl } = Constants.expoConfig?.extra || {}; // novo
 
 export async function fakeLogin(email: string, password: string): Promise<string> {
